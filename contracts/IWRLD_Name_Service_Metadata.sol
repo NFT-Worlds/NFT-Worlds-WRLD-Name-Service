85b056f4e50c75de21d34439a2c5cc826db397f5
arkdev <dev@nftworlds.com>
2022-06-10 11:50:55 -0700

    Added update to metadata.

--- contracts/IWRLD_Name_Service_Metadata.sol
+++ contracts/IWRLD_Name_Service_Metadata.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 interface IWRLD_Name_Service_Metadata is IERC165 {
-  function getMetadata(string memory _name, uint256 _expiresAt) external pure returns (string memory);
-  function getImage(string memory _name, uint256 _expiresAt) external pure returns (string memory);
+  function getMetadata(string memory _name, uint256 _expiresAt) external view returns (string memory);
+  function getImage(string memory _name, uint256 _expiresAt) external view returns (string memory);
 }
